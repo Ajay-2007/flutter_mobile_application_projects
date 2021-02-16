4184c799b7f8b943236153e5ab04c924790f802c
Ajay-2007 <iit2017040@iiita.ac.in>
2021-02-16 09:38:33 +0530

    modified home.dart for tflite functions

--- deep_learning_applications_with_flutter/catvsdog_classifier/lib/main.dart
+++ deep_learning_applications_with_flutter/catvsdog_classifier/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:catvsdog_classifier/splashscreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +9,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Cat vs Dog',
+      home: MySplash(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
