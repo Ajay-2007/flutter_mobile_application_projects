99240abdc7453bdfc1b43e4bf4b22f27bebcce94
Ajay-2007 <iit2017040@iiita.ac.in>
2021-02-17 15:10:46 +0530

    completed flower_classifier flutter app

--- deep_learning_applications_with_flutter/flower_classifier/lib/home.dart
+++ deep_learning_applications_with_flutter/flower_classifier/lib/home.dart
@@ -26,7 +26,7 @@ class _HomeState extends State<Home> {
   classifyImage(File image) async {
     var output = await Tflite.runModelOnImage(
       path: image.path,
-      numResults: 2,
+      numResults: 5,
       threshold: 0.5,
       imageMean: 127.5,
       imageStd: 127.5,
@@ -40,7 +40,7 @@ class _HomeState extends State<Home> {
 
   loadModel() async {
     await Tflite.loadModel(
-        model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+        model: 'assets/flower_classifier_model.tflite', labels: 'assets/labels.txt');
   }
 
   @override
@@ -181,7 +181,7 @@ class _HomeState extends State<Home> {
                           ),
                           SizedBox(height: 5),
                           GestureDetector(
-                            onTap: pickImage,
+                            onTap: pickGalleryImage,
                             child: Container(
                               width: MediaQuery.of(context).size.width - 180,
                               alignment: Alignment.center,
